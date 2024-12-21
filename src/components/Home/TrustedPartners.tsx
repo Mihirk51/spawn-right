e1ed5a7ea1e95fb01fcfcb1ff1faa74b06d2cb22
Mihir <mihirk51@gmail.com>
2024-12-21 13:58:46 -0600

    ui: add logos, remove later for obvious copyright reasons

--- src/components/Home/TrustedPartners.tsx
+++ src/components/Home/TrustedPartners.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
 export const TrustedPartners = () => {
+//remove these logos later for obvious copyright reasons
   return (
     <div className="mt-20">
       <h3 className="text-white text-xl mb-8">We are trusted by -</h3>
       <div className="flex items-center justify-between">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BLAST_logo.svg/2560px-BLAST_logo.svg.png" 
+        <img src="https://img-cdn.hltv.org/eventlogo/PocIgc9NQuXVubxA88zyEa.svg?ixlib=java-2.1.0&s=911e6cb16ddb42a8bbbca5b84497aa6d" 
              alt="Blast" 
-             className="h-8 opacity-50 hover:opacity-100 transition-opacity" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/ESL_Logo_2019.svg/2560px-ESL_Logo_2019.svg.png" 
+             className="h-20 opacity-100" />
+        <img src="https://img-cdn.hltv.org/eventlogo/ZMmU3y7hAV248CmzgxsohP.png?ixlib=java-2.1.0&w=50&s=fac3655d6c2a1abf92dfb3ef80cfc504" 
              alt="ESL" 
-             className="h-8 opacity-50 hover:opacity-100 transition-opacity" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Electronic_Arts_logo.svg/2560px-Electronic_Arts_logo.svg.png" 
-             alt="EA" 
-             className="h-8 opacity-50 hover:opacity-100 transition-opacity" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Valorant_logo_-_pink_color_version.svg/2560px-Valorant_logo_-_pink_color_version.svg.png" 
-             alt="Valorant" 
-             className="h-8 opacity-50 hover:opacity-100 transition-opacity" />
+             className="h-20 opacity-100" />
+        <img src="https://img-cdn.hltv.org/teamlogo/yeXBldn9w8LZCgdElAenPs.png?ixlib=java-2.1.0&w=50&s=15eaba0b75250065d20162d2cb05e3e6" 
+             alt="vita" 
+             className="h-20 opacity-100" />
+        <img src="https://img-cdn.hltv.org/teamlogo/zbcwVqDX-cVjB7EidzNoPd.png?ixlib=java-2.1.0&w=100&s=5d6488f42991807e0d921d0290c711ab 2x" 
+             alt="faze" 
+             className="h-20 opacity-100" />
       </div>
     </div>
   );
