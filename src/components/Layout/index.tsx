9a1f6e18effb594768e2d21d369d918b1dd14566
Mihir <mihirk51@gmail.com>
2024-12-22 19:28:23 -0600

    ui: some renames

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -13,7 +13,7 @@ export const Layout: React.FC = () => {
   }, [location.pathname, logNavigation]);
 
   return (
-    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(src/assets/backgroud_figma.png)' }}>
+    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(./src/assets/backgroud.png)' }}>
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 py-8">
         <Outlet />
