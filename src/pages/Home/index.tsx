3a8d77c8b70cd49f08f7e681a6545a1cf2a3143e
gulshansinghAIT <gulshankumar_22224@aitpune.edu.in>
2025-02-25 22:48:49 +0530

    made homepade responsive

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,11 +47,11 @@ export const Home = () => {
   return (
     <div className="space-y-8">
       <Hero />
-      <div className="grid grid-cols-12 gap-8">
-        <div className="col-span-8">
+      <div className="grid  grid-cols-12 gap-8">
+        <div className=" col-span-12 md:col-span-8">
           <TournamentsList/>
         </div>
-        <div className="col-span-4">
+        <div className="col-span-12 md:col-span-4">
           <LiveMatches/>
           <Statistics />
         </div>
